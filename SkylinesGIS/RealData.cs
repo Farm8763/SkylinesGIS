17c1dd7312dd14481d221bde6137b554b27c08ff
Farm8763 <rmcneely@teksavvy.com>
2015-03-29 10:23:47 -0400

    Added straight road building

--- SkylinesGIS/RealData.cs
+++ SkylinesGIS/RealData.cs
@@ -5,6 +5,7 @@ using ICities;
 using System;
 using UnityEngine;
 using System.ComponentModel;
+using ColossalFramework.Math;
 
 namespace SkylinesGIS
 {
@@ -21,15 +22,77 @@ namespace SkylinesGIS
             get { return "This mod allows for the importing of GIS data into Skylines."; }
         }
     }
-    
 
     public class LoadingExtension : LoadingExtensionBase
     {
         public override void OnLevelLoaded(LoadMode mode)
         {
-            dumpAllBuildings();
-            dumpAllVehicles();
-            spawnVehicle();
+            //dumpAllBuildings();
+            //dumpAllVehicles();
+            //spawnVehicle();
+            //roads/paths
+            //dumpAllNetInfo();
+            BuildingInfo cemetery = PrefabCollection<BuildingInfo>.GetPrefab(310);
+            BuildingInfo clinic = PrefabCollection<BuildingInfo>.GetPrefab(306);
+            BuildingInfo hospital = PrefabCollection<BuildingInfo>.GetPrefab(307);
+            BuildingInfo medicalCenter = PrefabCollection<BuildingInfo>.GetPrefab(308);
+            BuildingInfo fireStation = PrefabCollection<BuildingInfo>.GetPrefab(92);
+            BuildingInfo fireHouse = PrefabCollection<BuildingInfo>.GetPrefab(93);
+            NetInfo pedestrianPavement = PrefabCollection<NetInfo>.GetPrefab(4);
+            NetInfo basicRoad = PrefabCollection<NetInfo>.GetPrefab(38);
+            NetInfo gravelRoad = PrefabCollection<NetInfo>.GetPrefab(41);
+            Vector3 startPos = new Vector3(0, 0, 0);
+            Vector3 endPos = new Vector3(600, 0, 600);
+            //buildRoad(startPos, endPos, 38);
+            //buildRoad(startPos, new Vector3(800,0,800), 38);
+        }
+        public void buildRoad(Vector3 startVector, Vector3 endVector, uint prefabNumber)
+        {
+            int maxSegments = 3;
+            bool test = false;
+            bool visualize = false;
+            bool autoFix = true;
+            bool needMoney = false;
+            bool invert = false;
+            bool switchDir = false;
+            ushort relocateBuildingID = 0;
+            ushort firstNode;
+            ushort lastNode;
+            ushort startNode;
+            ushort endNode;
+            ushort segment;
+            int cost;
+            int productionRate;
+
+            NetInfo netInfo = PrefabCollection<NetInfo>.GetPrefab(prefabNumber);
+            float startHeight = NetSegment.SampleTerrainHeight(netInfo, startVector, false);
+            float endHeight = NetSegment.SampleTerrainHeight(netInfo, endVector, false);
+
+            NetTool.ControlPoint startControlPt = new NetTool.ControlPoint();
+            NetTool.ControlPoint endControlPt = new NetTool.ControlPoint();
+            
+            startVector.y = startHeight;
+            startControlPt.m_position = startVector;
+            endVector.y = endHeight;
+            endControlPt.m_position = endVector;
+
+            NetTool.CreateNode(netInfo, startControlPt, startControlPt, startControlPt, NetTool.m_nodePositionsSimulation,
+                0, false, false, false, false, false, false, (ushort)0, out startNode, out segment, out cost, out productionRate);
+
+            NetTool.CreateNode(netInfo, endControlPt, endControlPt, endControlPt, NetTool.m_nodePositionsSimulation, 
+                0, false, false, false, false, false, false, (ushort)0, out endNode, out segment, out cost, out productionRate);
+
+            startControlPt.m_node = startNode;
+            endControlPt.m_node = endNode;
+
+            NetTool.ControlPoint midControlPt = endControlPt;
+            midControlPt.m_position = (startControlPt.m_position + endControlPt.m_position) * 0.5f;
+            midControlPt.m_direction = VectorUtils.NormalizeXZ(midControlPt.m_position - startControlPt.m_position);
+            endControlPt.m_direction = VectorUtils.NormalizeXZ(endControlPt.m_position - midControlPt.m_position);
+
+            NetTool.CreateNode(netInfo, startControlPt, midControlPt, endControlPt, NetTool.m_nodePositionsSimulation,
+                 maxSegments, test, visualize, autoFix, needMoney, invert, switchDir, relocateBuildingID, out firstNode,
+                 out lastNode, out segment, out cost, out productionRate);
         }
 
         public void spawnVehicle()
@@ -38,9 +101,9 @@ namespace SkylinesGIS
             VehicleManager vehicleManager = Singleton<VehicleManager>.instance;
             SimulationManager simulationManager = Singleton<SimulationManager>.instance;
             VehicleInfo vehicleInfo = PrefabCollection<VehicleInfo>.GetPrefab(0);
-       
+            Vector3 position = new Vector3(0, 0, 0);
             bool vehicleCreated = vehicleManager.CreateVehicle(out newVehicle, ref simulationManager.m_randomizer,
-                vehicleInfo, Camera.main.transform.position, 
+                vehicleInfo, position, 
                 TransferManager.TransferReason.Single1, false, false);
 
             //VehicleInfo newInfo = vehicleManager.m_vehicles.m_buffer[(int)newVehicle].Info;
@@ -93,6 +156,15 @@ namespace SkylinesGIS
             {
                 BuildingInfo aBuildingInfo = PrefabCollection<BuildingInfo>.GetPrefab(x);
                 dumpObject(aBuildingInfo, "BuildingInfo " + x + ":");
+            }
+        }
+        public void dumpAllNetInfo()
+        {
+            int count = PrefabCollection<NetInfo>.LoadedCount();
+            for (uint x = 0; x < count; x += 1)
+            {
+                NetInfo aNetInfo = PrefabCollection<NetInfo>.GetPrefab(x);
+                dumpObject(aNetInfo, "aNetInfo " + x + ":");
             }
         }
         public void dumpAllVehicles()
